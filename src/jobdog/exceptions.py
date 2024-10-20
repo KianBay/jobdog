5d78fef5dcec15e3c725a0d439accf7e675d2fb9
Kian Bay <59806136+KianBay@users.noreply.github.com>
2024-10-21 01:41:20 +0200

    feat: add sanitizeurlerror

--- src/jobdog/exceptions.py
+++ src/jobdog/exceptions.py
@@ -1,11 +1,18 @@
 class JobDogException(Exception):
     """Base exception for JobDog"""
 
+
+class JobDogSanitizeUrlError(JobDogException):
+    """Raised when there's an error sanitizing the URL"""
+
+
 class FetchError(JobDogException):
     """Raised when there's an error fetching job details"""
 
+
 class ParserError(JobDogException):
     """Raised when there's an error parsing job details"""
+
 
 class UnsupportedProviderError(JobDogException):
     """Raised when no parser is found for a given URL"""
