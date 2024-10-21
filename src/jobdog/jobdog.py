59fa09d8da8dbe3e67593e00646f6d34b02792d8
Kian Bay <59806136+KianBay@users.noreply.github.com>
2024-10-21 03:49:44 +0200

    refactor: use httpx instead of curl_cffi

--- src/jobdog/jobdog.py
+++ src/jobdog/jobdog.py
@@ -1,26 +1,32 @@
 from typing import Optional, Dict
-from curl_cffi import requests
+from httpx import Client
 from .exceptions import FetchError
 from .logger import logger
 from .models.job_listing import JobListing
 from .providers.utils import get_parser
 from .providers.base import BaseParser
 
+
 class JobDog:
-    def __init__(self, impersonate: str = "chrome124", proxies: Optional[Dict[str, str]] = None, headers: Optional[Dict[str, str]] = None, timeout: int = 30) -> None:
-        self.impersonate = impersonate
-        self.proxies = proxies or {}
+    def __init__(
+        self,
+        mounts: Optional[Dict[str, str]] = None,
+        headers: Optional[Dict[str, str]] = None,
+        timeout: int = 30,
+    ) -> None:
+        self.mounts = mounts or {}
         self.headers = headers or {}
         self.timeout = timeout
-        self.session = self._create_session()
+        self.http_client = self._create_http_client()
 
-    def _create_session(self) -> requests.Session:
-        logger.info(f"Creating session with impersonate: {self.impersonate}, proxies: {self.proxies}, headers: {self.headers}, timeout: {self.timeout}")
-        return requests.Session(
-            impersonate=self.impersonate,
-            proxies=self.proxies,
+    def _create_http_client(self) -> Client:
+        logger.info(
+            f"Creating client: {self.mounts}, headers: {self.headers}, timeout: {self.timeout}"
+        )
+        return Client(
+            mounts=self.mounts,
             headers=self.headers,
-            timeout=self.timeout
+            timeout=self.timeout,
         )
 
     def fetch_details(self, url: str) -> JobListing:
@@ -28,7 +34,7 @@ class JobDog:
             logger.info(f"Fetching details for {url}")
             parser: BaseParser = get_parser(url)
             sanitized_url = parser.sanitize_url(url)
-            response = self.session.get(sanitized_url)
+            response = self.http_client.get(sanitized_url)
             response.raise_for_status()
             job_details: JobListing = parser.parse_html(response.text)
             job_details.job_listing_url = sanitized_url
@@ -37,27 +43,22 @@ class JobDog:
             logger.error(f"Error fetching {url}: {str(e)}")
             raise FetchError(f"Failed to fetch URL: {url}. Error: {str(e)}")
 
-    def set_impersonate(self, impersonate: str) -> None:
-        self.impersonate = impersonate
-        self.session = self._create_session()
-
-    def set_proxies(self, proxies: Dict[str, str]) -> None:
-        self.proxies = proxies
-        self.session = self._create_session()
+    def set_mounts(self, mounts: Dict[str, str]) -> None:
+        self.mounts = mounts
+        self.http_client = self._create_http_client()
 
     def set_headers(self, headers: Dict[str, str]) -> None:
         self.headers.update(headers)
-        self.session.headers.update(headers)
+        self.http_client.headers.update(headers)
 
     def set_timeout(self, timeout: int) -> None:
         self.timeout = timeout
-        self.session.timeout = timeout
-
+        self.http_client.timeout = timeout
 
     def close(self) -> None:
         logger.info("Attempting to close session")
-        if self.session:
-            self.session.close()
+        if self.http_client:
+            self.http_client.close()
             logger.info("Session closed")
 
     def __enter__(self) -> "JobDog":
