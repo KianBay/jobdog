a1440bf668a9e523232c27392c1eb0b5b145dee3
Kian Bay <59806136+KianBay@users.noreply.github.com>
2024-10-22 05:24:05 +0200

    tests: add test for parsing greenhosue

--- tests/providers/greenhouse_test.py
+++ tests/providers/greenhouse_test.py
@@ -36,7 +36,7 @@ GREENHOUSE_PARSE_TEST_CASES = [
     (
         GREENHOUSE_SHORT_URLS["scaleai"],
         {
-            "job_title": "Software Engineer, AI",
+            "job_title": "Senior/Staff Machine Learning Research Scientist Generative AI",
             "company_name": "Scale AI",
             "job_description": "You will be involved end-to-end from the inception and planning of new research agendas. You'll be creating high quality datasets, implementing models and associated training and evaluation stacks, producing high caliber publications in the form of peer-reviewed journal articles, blogs, white papers, and internal presentations & documentation.",
             "location": ["San Francisco, CA", "Seattle, WA", "New York, NY"],
@@ -70,4 +70,21 @@ def test_greenhouse_sanitize_url_error(invalid_url: str):
 @pytest.mark.vcr()
 def test_greenhouse_parse_html(url: str, expected_data: dict):
     parser = GreenhouseParser()
-    pass
+    resp = sync_http_client.get(url)
+    job_listing = parser.parse_html(resp.text)
+
+    logger.info(f"Response code: {resp.status_code}")
+    logger.info(f"Response text length: {len(resp.text)}")
+
+    assert isinstance(job_listing, JobListing)
+    assert job_listing.job_title == expected_data["job_title"]
+    assert job_listing.company_name == expected_data["company_name"]
+    assert expected_data["job_description"] in job_listing.job_description
+    logger.info(
+        f"Job description first 100 characters: {job_listing.job_description[:100]  }"
+    )
+    logger.info(
+        f"Job description last 100 characters: {job_listing.job_description[-100:]  }"
+    )
+    for i, location in enumerate(expected_data["location"]):
+        assert location == job_listing.location[i]
